fe82c945922a892095117c89f79240730f9bc148
Pna2791 <pna2791@gmail.com>
2022-05-07 18:43:14 +0700

    clone data comment

--- process_data/clone_data.py
+++ process_data/clone_data.py
@@ -4,6 +4,7 @@ import torch
 length = 120
 
 
+# Tạo khung data trắng
 z_acc = torch.zeros(length, 6, 3, dtype=torch.float32)
 z_rot = torch.zeros(length, 6, 3, 3, dtype=torch.float32)
 z_rot[:, :] = torch.tensor([[1, 0, 0],
@@ -42,6 +43,7 @@ def euler2matrix(euler):
     return ch_rotz(yaw).dot(ch_roty(roll)).dot(ch_rotx(pitch))
 
 
+# các công thức tính theo thời gian
 def omega_t(t):
     if t<31:
         return gama*t/60
@@ -61,6 +63,7 @@ def acc_t(t):
     return -0.8*pi
 
 
+# Tạo ra các dữ liệu ảo
 acc = []
 rot = []
 for i in range(1, 61):
@@ -68,11 +71,14 @@ for i in range(1, 61):
     a = -acc_t(i)
     a_x = -sin(alpha) * a
     a_y = cos(alpha) * a
+
+    # điền các số liệu cho gia tốc và euler
     acc_global = np.array([0, a, 0])
     euler = [0, 0, -pi/2]
     matrix = euler2matrix(euler)
     # print(alpha*180/pi)
 
+    # Chuyển gia tốc sang local hoặc lưu global
     # acc.append(np.linalg.inv(matrix).dot(acc_global))
     acc.append(acc_global)
     rot.append(matrix)
@@ -80,7 +86,7 @@ for i in range(1, 61):
 acc = torch.tensor(np.array(acc))
 rot = torch.tensor(np.array(rot))
 
-
+# thêm các giá trị vừa tạo vào frame 30 - 90
 start_frame = 30
 # z_acc[start_frame:start_frame+60, 0, :] = acc
 z_rot[start_frame:start_frame+60, 0, :, :] = rot
